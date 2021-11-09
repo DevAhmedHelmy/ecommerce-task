a1e43346d7b4eb467c639b45f2115be891f3e90a
ahmed helmy <helmyahmed248@gmail.com>
2021-11-09 18:10:56 +0200

    add product module

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,9 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Passport::routes();
-
-        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
     }
 }
